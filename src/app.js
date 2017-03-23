4a6aa2ecb448d0f020e41673756ccf06ee50858a
stewart leach <stewartleach@Naya-Las-MacBook-Pro.local>
2017-03-23 23:29:05 +1100

    couchdb connection test working

--- src/app.js
+++ src/app.js
@@ -24,11 +24,29 @@ var options = {
   path: '/',
   method: 'GET'
 };
-http.request(options, function(res){
-	console.log("logging response");
-	console.log(res);
+// http.request(options, function(res){
+// 	console.log("logging response");
+// 	console.log(res);
+// });
+var req = http.get(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+  // Buffer the body entirely for processing as a whole.
+  var bodyChunks = [];
+  res.on('data', function(chunk) {
+    // You can process streamed parts here...
+    bodyChunks.push(chunk);
+  }).on('end', function() {
+    var body = Buffer.concat(bodyChunks);
+    console.log('BODY: ' + body);
+    // ...and/or process the entire body here.
+  })
 });
 
+req.on('error', function(e) {
+  console.log('ERROR: ' + e.message);
+});
 
 app.get('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
